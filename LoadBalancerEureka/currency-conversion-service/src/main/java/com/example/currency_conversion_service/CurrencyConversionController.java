5c3caa4b1472a11384a876b47fc6432e73b094f6
Sandesh <sandeshgharge@gmail.com>
2024-09-10 15:55:57 +0200

    API Gateway started working unexpectedly, Tracing tested with zipkin

--- LoadBalancerEureka/currency-conversion-service/src/main/java/com/example/currency_conversion_service/CurrencyConversionController.java
+++ LoadBalancerEureka/currency-conversion-service/src/main/java/com/example/currency_conversion_service/CurrencyConversionController.java
@@ -4,14 +4,35 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Class defined to get tracing for request which are without Fiegn configuration
+ * This class should be preferably defined in another method but used here as a short cut with **Micrometer**
+ * 
+ * 
+ */
+@Configuration(proxyBeanMethods = false)
+class RestTemplateConfiguration {
+    
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
+
 @RestController
 public class CurrencyConversionController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	/**
 	 * This tedious can be replaced by the feature Spring cloud provides named FEIGN
@@ -31,7 +52,8 @@ public class CurrencyConversionController {
 		uriVariables.put("from", from);
 		uriVariables.put("to", to);
 		ResponseEntity<CurrencyConversion> respEntity = 
-				new RestTemplate().getForEntity(
+				// new RestTemplate().getForEntity(
+				restTemplate.getForEntity(
 						"http://localhost:8000/currency-exchange/from/{from}/to/{to}",
 						CurrencyConversion.class,
 						uriVariables
--- LoadBalancerEureka/currency-exchange-service/src/main/java/com/example/currency_exchange_service/CurrencyExchangeController.java
+++ LoadBalancerEureka/currency-exchange-service/src/main/java/com/example/currency_exchange_service/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.example.currency_exchange_service;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 	@Autowired 
 	private Environment environment;
 	
@@ -27,6 +30,7 @@ public class CurrencyExchangeController {
 		if(currencyExchange == null) {
 			throw new RuntimeException("Unable to find data for " + from + " and " + to);
 		}
+		logger.info("Request for From : {}, To : {}", from, to);
 		currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
 		return currencyExchange;
 	}
